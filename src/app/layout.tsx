3a5c126ff83d3b7b6bfff0b0dc06c4f63b305451
swatbyswat <swatilakha25@gmail.com>
2023-05-20 13:00:15 +0530

    meta

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export const metadata = {
   title: {
     default: 'Zenversee | Coming Soon',
     template: '%s',
-  }
+  },
   description: 'Unlock the power of innovation with Zenversee, a leading software development agency. Explore our range of cutting-edge SaaS products and discover tailored marketing solutions to elevate your business. Stay tuned as we prepare to launch our website, bringing you groundbreaking technology and results-driven strategies for success.',
   keywords: ['zenversee','software developement agency','social media marketing agency','digital marketing','saas products'],
   creator: 'Zenversee',
